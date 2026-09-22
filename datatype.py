91ca89803956e49239d665706cacf096f18d1d00
Atul Yadav <atulyadavv17@gmail.com>
2026-09-22 23:59:23 +0530

    datatype

---
+++ datatype.py
@@ -0,0 +1,14 @@
+name = "atul"
+age = 23
+height= 6.3
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(height))
